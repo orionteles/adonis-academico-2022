4e8112cd03afe40fab38a886e9e48797154e16e9
Orion Teles de Mesquita <orionteles@gmail.com>
2022-10-05 21:51:33 -0300

    [ADD] :beers: Criando CRUD de Disciplinas

---
+++ app/Controllers/Http/DisciplinasController.ts
@@ -0,0 +1,49 @@
+// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+import Disciplina from "App/Models/Disciplina";
+
+export default class DisciplinasController {
+
+    index({request}){
+
+        const {nome, cursoId} = request.all()
+
+        const disciplinas = Disciplina.query()
+                                     .select(['id', 'nome', 'cursoId'])
+
+        if(cursoId){
+            disciplinas.where('cursoId', cursoId)
+        }
+
+        if(nome){
+            disciplinas.where('nome', nome)
+        }
+    
+        return disciplinas
+    }
+
+    show({request}) {
+        const id = request.param('id')
+        return Disciplina.findOrFail(id)
+    }
+
+    store({request}) {
+        const dados = request.only(['nome', 'cursoId'])
+        return Disciplina.create(dados)
+    }
+
+    async update({request}){
+        const id = request.param('id')
+        const disciplina = await Disciplina.findOrFail(id)
+
+        const dados = request.only(['nome', 'cursoId'])
+
+        return disciplina.merge(dados).save()
+    }
+
+    async destroy({request}){
+        const id = request.param('id')
+        const disciplina = await Disciplina.findOrFail(id)
+        return disciplina.delete()
+    }
+}
